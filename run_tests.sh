a603e363bab330b6627673a1a168d1c453816bba
Tim Booth <tim.booth@ed.ac.uk>
2016-08-31 18:34:20 +0100

    Sketch out the initial test framework.

---
+++ run_tests.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# When running the tests, we need to ensure Python picks up the right environment.
+# For this reason ,it's worth having a test wrapper.
+cd "`dirname $0`"
+
+export RUN_SLOW_TESTS=${RUN_SLOW_TESTS:-0}
+export RUN_NETWORK_TESTS=${RUN_NETWORK_TESTS:-1}
+
+#Test in Py3 only
+if [ "$*" == "" ] ; then
+    python3 -munittest discover
+else
+    set -e
+    python3 -munittest test.test_"$@"
+fi
+
+
+# Pyflakes is my favoured static analyser for regression testing because it
+# just looks at one file at a time, thought it wouldn't hurt to cast
+# pylint over the code too.
+PYFLAKES="/ifs/software/linux_x86_64/python/python_2.7.10_venv/bin/pyflakes"
+files_to_flake="bin/*.py"
+
+if [ "$*" == "" ] ; then
+    if [ -x "$PYFLAKES" ] ; then
+        for f in $files_to_flake ; do
+            echo "### Running pyflakes $f"
+            "$PYFLAKES" "$f"
+        done
+    else
+        echo "Unable to run $PYFLAKES!"
+    fi
+fi
