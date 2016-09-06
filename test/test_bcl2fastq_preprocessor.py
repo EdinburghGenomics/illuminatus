1d87b7a6209f5e399d85d5c3ee9ee1b1f89ded1f
Tim Booth <tim.booth@ed.ac.uk>
2016-09-06 18:11:20 +0100

    Staking a claim to the preprocessor module.

---
+++ test/test_bcl2fastq_preprocessor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import unittest
+import sys
+import glob
+
+# import stuff from ../ directory
+sys.path.insert(0,'../bin/')
+
+from RunInfo import RunInfo
+
+class TestBCL2FASTQPreprocessor(unittest.TestCase):
+
+    def test_(self):
+
+        self.assertEquals(lalalal)
