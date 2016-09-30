ff1de372e4de8d758845eb875228548e592e96f0
Tim Booth <tim.booth@ed.ac.uk>
2016-09-30 14:30:29 +0100

    Added more tests.  Everything passes.

--- test/test_sample_sheet_reader.py
+++ test/test_sample_sheet_reader.py
@@ -14,7 +14,7 @@ class TestSampleSheetReader(unittest.TestCase):
 
         ** I renamed this to get_index_lengths_by_lane()
     """
-
+    #Utility funcs
     def get_reader_for_sample_sheet(self, run_name):
         """Creates a new reader object from one of our test runs, which live in
            test/seqdata_examples.
@@ -26,6 +26,7 @@ class TestSampleSheetReader(unittest.TestCase):
 
         return SampleSheetReader(ssfile)
 
+    #Tests
     def test_missing_samplesheet(self):
         """Basic test that the exception gets propogated appropriately.
         """
@@ -44,4 +45,28 @@ class TestSampleSheetReader(unittest.TestCase):
         #And do they all have zero-length, being dummy indexes?
         self.assertCountEqual( ilbl.values(), [[0,0]] * 8  )
 
+    def test_varied_2500_run(self):
+        """Run 160607_D00248_0174_AC9E4KANXX has 8 lanes of various types.
+        """
+        r = self.get_reader_for_sample_sheet('160607_D00248_0174_AC9E4KANXX')
+        ilbl = r.get_index_lengths_by_lane()
+        self.assertEqual( sorted(ilbl.keys()), list('12345678') )
 
+        self.assertEqual( ilbl['1'], [8, 8] )
+        self.assertEqual( ilbl['2'], [8, 8] )
+        self.assertEqual( ilbl['3'], [6, 0] )
+        self.assertEqual( ilbl['4'], [8, 0] )
+        self.assertEqual( ilbl['5'], [0, 0] )
+        self.assertEqual( ilbl['6'], [0, 0] )
+        self.assertEqual( ilbl['7'], [0, 0] )
+        self.assertEqual( ilbl['8'], [8, 0] )
+
+    def test_miseq_run(self):
+        """Run 160603_M01270_0196_000000000-AKGDE has 1 lane, with single barcodes
+           length 10
+        """
+        r = self.get_reader_for_sample_sheet('160603_M01270_0196_000000000-AKGDE')
+        ilbl = r.get_index_lengths_by_lane()
+        self.assertEqual( sorted(ilbl.keys()), list('1') )
+
+        self.assertEqual( ilbl['1'], [10, 0] )
