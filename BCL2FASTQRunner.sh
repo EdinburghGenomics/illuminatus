78ca3b03c23eeed9aa0e1c8be094853445cd912c
Tim Booth <tim.booth@ed.ac.uk>
2017-01-27 16:20:28 +0000

    More fancy flocking, and tell the runner about our new cluster.

--- BCL2FASTQRunner.sh
+++ BCL2FASTQRunner.sh
@@ -1,26 +1,51 @@
 #!/bin/bash
+set -e ; set -u
 
 # Usage (normally called from driver.sh):
 #  cd output_dir
 #  /path/to/BCL2FASTQRunner.sh
 
+# SGE settings...
 #$ -cwd -v PATH -v LD_LIBRARY_PATH -sync yes -pe qc 8 -t 1-1 -q casava
 #$ -N demultiplexing  -o sge_output -e sge_output
 
-if [ -z "${SGE_TASK_ID:-}" ] ; then
-    #I humbly submit myself to the cluster.
-    mkdir -p ./sge_output
-    qsub "$0"
-    exit $?
+# SLURM settings...
+#SBATCH --wait #This might be unreliable, see slurm_tools/sbatch_wait.sh
+#SBATCH -p casava
+#SBATCH -c 8
+#SBATCH -J demultiplexing
+#SBATCH -o slurm_output/demultiplexing.%A.%a.out
+#SBATCH -e slurm_output/demultiplexing.%A.%a.err
+
+if [ ! -e /lustre/software ] ; then
+    if [ -z "${SGE_TASK_ID:-}" ] ; then
+        #I humbly submit myself to the (old) cluster.
+        mkdir -p ./sge_output
+        qsub "$0"
+        exit $?
+    fi
+else
+    if [ -z "${SLURM_JOB_ID:-}" ] ; then
+        #I humbly submit myself to the (new) cluster.
+        mkdir -p ./slurm_output
+        sbatch "$0"
+        exit $?
+    fi
 fi
 
 #Actual cluster job
 exec 2>&1 #Everything to stdout
 echo $PWD
-echo -e "\nSGE_TASK_ID=$SGE_TASK_ID\n"
+echo -e "\nSGE_TASK_ID=${SGE_TASK_ID:-}\n"
+echo -e "\nSLURM_JOB_ID=${SLURM_JOB_ID:-}\n"
 
 echo "Starting bcl2fastq per do_demultiplex.sh..."
 set -x
 
-PATH="/ifs/software/linux_x86_64/Illumina_pipeline/bcl2fastq2-v2.17.1.14-bin/bin/:$PATH"
+#Pick the appropriate bcl2fastq.
+if [ -e /lustre/software/bcl2fastq/bcl2fastq2-v2.17.1.14/ ] ; then
+    PATH="/lustre/software/bcl2fastq/bcl2fastq2-v2.17.1.14/bin/:$PATH"
+else
+    PATH="/ifs/software/linux_x86_64/Illumina_pipeline/bcl2fastq2-v2.17.1.14-bin/bin/:$PATH"
+fi
 source ./do_demultiplex.sh
--- sync_and_driver.sh
+++ sync_and_driver.sh
@@ -9,62 +9,86 @@ set -u
 
 # Normally, the driver will run from the CRON every 5 minutes and locking at the
 # project level will be used to prevent two instances interfering. Also, seeing an
-# incomplete project is not a problem because the file appear in the expected order.
+# incomplete project is not a problem because the files appear in the expected order.
+# But with RSync we can't guarantee that, so we must lock.
 
 
 # Settings in driver.sh to override.
 export SEQDATA_LOCATION=/lustre/seqdata
 export FASTQ_LOCATION=/lustre/fastqdata
-
 export LOG_DIR=~pipeline/illuminatus/logs
 
-# With RSYNC, we don't want two processes operating at once and also we don't want to
+# With RSYNC, we don't want two syncs operating at once and also we don't want to
 # let the driver run while RSYNC is running because it might copy the touch file
 # before all the data is done.
 #
 # For RapidQC I used 'ps' to see if the script was already running, but I think a
-# flock-based solution is actually neater.
+# flock-based solution is actually much neater.
 # Should really flock on something in /var/run/lock but that gets killed on reboot
 # and only root can re-make the file. So use /tmp instead.
+#
 
-# 1) Engage exclusive lock
-FLOCK_FILE="/tmp/flock_$(readlink -f "$0" | base64 -w 0)"
-if [ "${FLOCK_ON:-0}" != 1 ] ; then
-    if ! env FLOCK_ON=1 flock -n "$FLOCK_FILE" "$0" "$@" ; then
-        echo "Running $0 under flock failed."
-        exit 1
+# 1) Engage shared lock
+FLOCK_FILE="/tmp/flock_$(readlink -f "$0" | md5sum | awk '{print $1}')"
+if [ "${FLOCK_ON:-0}" = 0 ] ; then
+    # echo "Locking on $FLOCK_FILE, PID=$$"
+    (   flock -s -n 9 || exit 33
+        export FLOCK_ON=9
+        source "$0" "$@"
+    ) 9>"$FLOCK_FILE" ; rc="$?"
+    if [ "$rc" != 0 ] ; then
+        if [ "$rc" = 33 ] ; then
+            echo "Failed to gain shared lock, PID=$$"
+        else
+            echo "Script exited with error $rc"
+        fi
+        exit "$rc"
     fi
+    #Spawned copy ran, nothing more to do.
+    #echo "Exiting unlocked script, PID=$$"
     exit 0
 fi
+#echo "Locked on $FLOCK_FILE, PID=$$"
 
-# 2) Ensure that the directory is there for the rsync log
-mkdir -p "$LOG_DIR" 
-echo "=== Running $0 at `date` ===" >> "$LOG_DIR"/rsync.log
+# 2) Attempt to upgrade to exclusive lock, and run RSYNC if successful
+if flock -x -n $FLOCK_ON ; then
+    #echo "Entering exclusive section, PID=$$"
 
-# 3) Run rsync. But just running rsync on everything will take way too long, so we need a smarter strategy.
+    # 2a) Ensure that the directory is there for the rsync log
+    mkdir -p "$LOG_DIR"
+    echo "=== Running $0 at `date` ===" >> "$LOG_DIR"/rsync.log
 
-# 3a - Only look at the last 20 days fo runs
-newish_runs=($(find /ifs/seqdata/17*_*_*_* -maxdepth 0 -mindepth 0 -type d -mtime '-20'))
+    # Run rsync. But just running rsync on everything will take way too long, so we need a smarter strategy.
 
-echo "Starting rsync on ${newish_runs[@]}"
+    # 2b) Only look at the last 20 days fo runs
+    newish_runs=($(find /ifs/seqdata/17*_*_*_* -maxdepth 0 -mindepth 0 -type d -mtime '-20'))
 
-# 3b - Rsync just the bare bones of these
-rsync -av --exclude='Data/*' --exclude='Images/*' --exclude='Thumbnail_Images/*' --exclude='Logs/*' \
-    --exclude=RTAComplete.txt \
-    "${newish_runs[@]}" /lustre/seqdata >> "$LOG_DIR"/rsync.log
+    echo "Starting rsync on ${newish_runs[@]}" >> "$LOG_DIR"/rsync.log
 
-# 3c - Find any directory on /lustre where the RTAComplete.txt file is missing and do a full RSYNC
-#      if there is a corresponding RTAComplete.txt on /ifs.
-for dest_run in /lustre/seqdata/17*_*_*_* ; do
-    if [ ! -e "$dest_run/RTAComplete.txt" ] ; then
-        src_run="/ifs/seqdata/$(basename $dest_run)"
-        if [ -e "$src_run/RTAComplete.txt" ] ; then
-            #Now bring the whole run across. I considered explicitly copying RTAComplete.txt right at the end but
-            #it seems like overkill.
-            rsync -av "$src_run" /lustre/seqdata >> "$LOG_DIR"/rsync.log
+    # 2c) Rsync just the bare bones of these
+    rsync -av --exclude='Data/*' --exclude='Images/*' --exclude='Thumbnail_Images/*' --exclude='Logs/*' \
+        --exclude=RTAComplete.txt \
+        "${newish_runs[@]}" /lustre/seqdata >> "$LOG_DIR"/rsync.log
+
+    # 2d) Find any directory on /lustre where the RTAComplete.txt file is missing and do a full RSYNC
+    #     if there is a corresponding RTAComplete.txt on /ifs.
+    for dest_run in /lustre/seqdata/17*_*_*_* ; do
+        if [ ! -e "$dest_run/RTAComplete.txt" ] ; then
+            src_run="/ifs/seqdata/$(basename $dest_run)"
+            if [ -e "$src_run/RTAComplete.txt" ] ; then
+                #Now bring the whole run across. I considered explicitly copying RTAComplete.txt right at the end but
+                #it seems like overkill.
+                rsync -av "$src_run" /lustre/seqdata >> "$LOG_DIR"/rsync.log
+            fi
         fi
-    fi
-done
+    done
+
+    #echo "Leaving exclusive section, PID=$$"
+    # Return to shared lock
+    flock -s $FLOCK_ON
+else
+    #echo "Failed to gain exclusive lock, PID=$$"
+fi
 
 # 4) Aaaaand finally, we can run driver.sh
 $(dirname $(readlink -f "$0"))/driver.sh
