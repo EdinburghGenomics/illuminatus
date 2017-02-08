77f6406d742bfd8cb388c58b27a44478d1d11ad8
Tim Booth <tim.booth@ed.ac.uk>
2017-02-08 16:46:43 +0000

    samplesheet_fetch.sh now actually fetches the actual sample sheets

--- samplesheet_fetch.sh
+++ samplesheet_fetch.sh
@@ -4,6 +4,12 @@
 # be run in the folder where the sequencer data has been written.
 set -e ; set -u
 
+if [ -z "${FLOWCELLID:-}" ] ; then
+    #Try to determine flowcell ID by asking RunInfo.py
+    FLOWCELLID=`RunInfo.py | grep '^Flowcell:' | cut -f2 -d' '`
+fi
+
+
 # Firstly, SampleSheet.csv.0 needs to contain the original file from the
 # sequencer. It is technically possible to run the machine with no sample
 # sheet. In this case, the script will make an empty file.
@@ -17,21 +23,27 @@ if [ ! -e SampleSheet.csv.0 ] ; then
     ln -s SampleSheet.csv.0 SampleSheet.csv
 fi
 
-# At this point we may expect that SampleSheet.csv is a symlink
+# At this point we may expect that SampleSheet.csv is a symlink. Sanity check.
 test -L SampleSheet.csv
 
-# Find a candidate sample sheet, either by looking directly on the smb mount or by
-# smbclient or sftp or ...
+# Find a candidate sample sheet, from the files on /ifs/clarity (or wherever)
 
-# For now, just cheat.
-cat SampleSheet.csv > SampleSheet.csv.tmp
-candidate_ss="SampleSheet.csv.tmp"
+# Read the Genologics configuration with the same priorities as the LIMSQuery.py code.
+# This is a bit hacky, but it is effective.
+eval `grep -h '^FS_ROOT=' /etc/genologics.conf genologics.cfg genologics.conf ~/.genologicsrc ${GENOLOGICSRC:-/dev/null} 2>/dev/null`
+test -d "$FS_ROOT"
 
-if diff -q "$candidate_ss" SampleSheet.csv ; then
-    #Nothing to do. Maybe remove CANDIDATE_SS
+# The latest one that matches the flowvell ID is the one we want.
+# Or do we want to sort on some other criterion?
+candidate_ss=`find "$FS_ROOT/samplesheets_bcl2fastq_format" -name "*_${FLOWCELLID}.csv" -print0 | xargs -r0 ls -tr | tail -n 1`
+
+if [ ! -e "$candidate_ss" ] ; then
+    echo "No candidate replacement samplesheet under $FS_ROOT/samplesheets_bcl2fastq_format"
+elif diff -q "$candidate_ss" SampleSheet.csv ; then
+    #Nothing to do.
     echo "SampleSheet.csv is already up-to-date"
 else
-    #No need to test file existence before writing - just use noclobber
+    #Using noclobber to attempt writing to files until we find an unused name
     set noclobber
     counter=1
     while ! cat "$candidate_ss" > "SampleSheet.csv.$counter" ; do
@@ -41,7 +53,7 @@ else
         test $counter -lt 1000
     done
 
-    ln -vsf "SampleSheet.csv.$counter" SampleSheet.csv #Should be safe - we checked it was only a link
+    ln -sf "SampleSheet.csv.$counter" SampleSheet.csv #Should be safe - we checked it was only a link
+    echo "SampleSheet.csv is now linked to new SampleSheet.csv.$counter"
 fi
 
-rm "$candidate_ss"
--- test/test_samplesheet_fetch.py
+++ test/test_samplesheet_fetch.py
@@ -19,6 +19,8 @@ FETCH = os.path.abspath(os.path.dirname(__file__) + '/../samplesheet_fetch.sh')
 class TestSamplesheetFetch(unittest.TestCase):
 
     def setUp(self):
+        self.maxDiff = None
+
         #Always make a fresh temporary folder to be working in
         oldcwd = os.getcwd()
         temp_dir = mkdtemp()
@@ -28,18 +30,52 @@ class TestSamplesheetFetch(unittest.TestCase):
             os.chdir(oldcwd)
             rmtree(temp_dir)
 
-        self.addCleanup(cleanup)
+        if os.environ.get("KEEPTMP"):
+            print(temp_dir)
+        else:
+            self.addCleanup(cleanup)
 
         self.last_stdout = self.last_stderr = None
+
+        #The script will find sample sheets in here...
+        self.ss_dir = "fs_root/samplesheets_bcl2fastq_format"
+        with open('genologics.conf', 'x') as cfh:
+            print("FS_ROOT=" + temp_dir + "/fs_root", file=cfh)
+        os.makedirs(self.ss_dir)
+        os.environ['GENOLOGICSRC'] = temp_dir + '/genologics.conf'
+
+        #The flowcell ID will always be XXXX
+        os.environ['FLOWCELLID'] = 'XXXX'
 
     def tearDown(self):
         #Cleanup of temp_dir is handled by the callback hook above
         pass
 
     def test_replace_original(self):
-        """When this script sees the run folder for the first time.
+        """When this script sees the run folder for the first time,
+           and there is a replacement available.
         """
 
+        touch('SampleSheet.csv')
+        touch(self.ss_dir + '/foo_XXXX.csv')
+        touch(self.ss_dir + '/bar_XXXX.csv', 'this one')
+
+        self.run_fetch()
+
+        self.assertTrue(os.path.isfile('SampleSheet.csv.0'))
+        self.assertTrue(os.path.isfile('SampleSheet.csv.1'))
+        self.assertEqual(os.readlink('SampleSheet.csv'), 'SampleSheet.csv.1')
+
+        self.assertEqual(self.last_stdout[0], "SampleSheet.csv renamed as SampleSheet.csv.0")
+        self.assertEqual(self.last_stdout[2], "SampleSheet.csv is now linked to new SampleSheet.csv.1")
+
+        with open("SampleSheet.csv") as fh:
+            self.assertEqual(fh.read().rstrip(), 'this one')
+
+    def test_keep_original(self):
+        """When this script sees the run folder for the first time,
+           and there is no replacement available.
+        """
         touch('SampleSheet.csv')
 
         self.run_fetch()
@@ -48,6 +84,8 @@ class TestSamplesheetFetch(unittest.TestCase):
         self.assertEqual(os.readlink('SampleSheet.csv'), 'SampleSheet.csv.0')
 
         self.assertEqual(self.last_stdout[0], "SampleSheet.csv renamed as SampleSheet.csv.0")
+        self.assertEqual(self.last_stdout[1][:36], "No candidate replacement samplesheet")
+
 
     def test_none_found(self):
         """This shouldn't happen in practise. May want to reconsider the behaviour
