d4fbe73a7899785cae793f3e98662c72a1f2719d
Tim Booth <tim.booth@ed.ac.uk>
2017-05-05 15:48:38 +0100

    Make this work for MiSEQ runs.

--- test/end_to_end/slim_a_run.sh
+++ test/end_to_end/slim_a_run.sh
@@ -2,33 +2,55 @@
 set -ue
 
 # Takes a run folder and makes a mini version of it for you to test on.
-RUN_ID="${1:?Give me a run to slim}"
+# eg:
+# $ test/end_to_end/slim_a_run.sh 170221_K00166_0183_AHHT3HBBXX ~/test_seqdata
+
+RUN_PATH="${1:?Give me a run to slim}"
 DEST="${2:-.}"
 
 # If RUN_ID contains no /, assume /lustre/seqdata
-if [[ ! "$RUN_ID" =~ / ]] ; then
-    RUN_ID=/lustre/seqdata/"$RUNID"
+if [[ ! "$RUN_PATH" =~ / ]] ; then
+    RUN_PATH=/lustre/seqdata/"$RUN_PATH"
 fi
+RUN_ID="`basename $RUN_PATH`"
 
 # Now DEST...
-DEST="$DEST/`basename $RUN_ID`"
+DEST="$DEST/$RUN_ID"
 
-echo "Slimming down $RUN_ID --> $DEST"
-exit 1
-mkdir /ifs/runqc/test_seqdata/$RUN_ID/
-mkdir /ifs/runqc/test_seqdata/$RUN_ID/Data/
-mkdir /ifs/runqc/test_seqdata/$RUN_ID/Data/Intensities/
-mkdir /ifs/runqc/test_seqdata/$RUN_ID/Data/Intensities/BaseCalls/
-mkdir /ifs/runqc/test_seqdata/$RUN_ID/Data/Intensities/BaseCalls/L00{1,2,3,4,5,6,7,8}
-cp /ifs/seqdata/$RUN_ID/* /ifs/runqc/test_seqdata/$RUN_ID/
-cp /ifs/seqdata/$RUN_ID/Data/* /ifs/runqc/test_seqdata/$RUN_ID/Data
-cp /ifs/seqdata/$RUN_ID/Data/Intensities/* /ifs/runqc/test_seqdata/$RUN_ID/Data/Intensities/
-cp /ifs/seqdata/$RUN_ID/Data/Intensities/BaseCalls/* /ifs/runqc/test_seqdata/$RUN_ID/Data/Intensities/BaseCalls/
+if [ -e "$DEST" ] ; then
+  echo "$DEST already exists. Remove it first."
+  exit 1
+fi
 
-LANE=1; 
-for LANE in $(seq 1 8); do \
-  cp /ifs/seqdata/$RUN_ID/Data/Intensities/BaseCalls/L00$LANE/s_${LANE}_1101.filter /ifs/runqc/test_seqdata/$RUN_ID/Data/Intensities/BaseCalls/L00$LANE/; \
-    for f in $(seq 1 310); do \
-        mkdir /ifs/runqc/test_seqdata/$RUN_ID/Data/Intensities/BaseCalls/L00$LANE/C$f.1; \
-            cp /ifs/seqdata/$RUN_ID/Data/Intensities/BaseCalls/L00$LANE/C$f.1/s_${LANE}_1101.bcl.gz /ifs/runqc/test_seqdata/$RUN_ID/Data/Intensities/BaseCalls/L00$LANE/C$f.1/s_${LANE}_1101.bcl.gz; done ;done
+echo "Slimming down $RUN_PATH --> $DEST"
 
+LANES="`ls "$RUN_PATH"/Data/Intensities/BaseCalls | grep -x L...`"
+echo "Making directories for `wc -w <<<$LANES` lanes."
+for lane in $LANES ; do
+    mkdir -p "$DEST"/Data/Intensities/BaseCalls/$lane
+done
+
+# Copy files, but not folders, at various levels
+for p in . Data Data/Intensities Data/Intensities/BaseCalls ; do
+    echo "Copying all regular files from $RUN_PATH/$p"
+    cp "$RUN_PATH/$p"/* "$DEST/$p" 2>/dev/null || true
+done
+
+for lane in $LANES ; do
+  cp -v "$RUN_PATH"/Data/Intensities/BaseCalls/$lane/s_*_1101.filter "$DEST"/Data/Intensities/BaseCalls/$lane/
+  CYCLES="`ls "$RUN_PATH"/Data/Intensities/BaseCalls/$lane | grep -x 'C[0-9]\+.1'`"
+  echo "Copying `wc -w <<<$CYCLES` cyles of bcl[.gz] files for tile 1101 of $lane..."
+  for cycle in $CYCLES ; do
+    mkdir "$DEST"/Data/Intensities/BaseCalls/$lane/$cycle
+    cp "$RUN_PATH"/Data/Intensities/BaseCalls/$lane/$cycle/s_*_1101.bcl* "$DEST"/Data/Intensities/BaseCalls/$lane/$cycle/
+    #We may or may not have these files
+    cp "$RUN_PATH"/Data/Intensities/BaseCalls/$lane/$cycle/s_*_1101.stats "$DEST"/Data/Intensities/BaseCalls/$lane/$cycle/ 2>/dev/null || true
+  done
+done
+
+# Finally copy the SampleSheet.csv to SampleSheet.csv.OVERRIDE so Illuminatus won't try
+# to replace it.
+echo Creating "$DEST"/SampleSheet.csv.OVERRIDE
+cat "$DEST"/SampleSheet.csv > "$DEST"/SampleSheet.csv.OVERRIDE
+
+echo DONE
