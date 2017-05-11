22ec670aed074f7bf400a9b2c668899e8a5428aa
Tim Booth <tim.booth@ed.ac.uk>
2017-05-11 13:54:16 +0100

    Implement the cleanup component as per the design spec and add test cases for it.

---
+++ BCL2FASTQCleanup.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+import os, sys, re
+from glob import glob
+import time
+
+def main(output_dir, *lanes):
+    """Usage: BCL2FASTQCleanup.py <output_dir> <lanes_list>
+
+    Given an output folder, clean up old FASTQ files ready for re-demultiplexing.
+    Also clean [other stuff]. See design criteria at:
+      https://genowiki.is.ed.ac.uk/pages/viewpage.action?pageId=319660973
+    """
+
+    output_dir = os.path.abspath(output_dir)
+
+    # Open the log. This will barf on non-existent output_dir.
+    with open(os.path.join(output_dir, 'cleanup.log'), 'a') as log_fh:
+        log = lambda m: print(m, file=log_fh)
+        die = lambda m: print("# ERROR: %s" % m, file=log_fh) or exit(m)
+
+        log("# %s" % sys.argv[0])
+        log("# cleaning lanes %s in %s on %s" % (
+                              lanes,
+                                 output_dir,
+                                       time.strftime('%Y-%m-%d %H:%M', time.localtime()) ))
+
+        # Sanity checking...
+        if not lanes:
+            die("No lanes specified to process.")
+
+        for l in lanes:
+            if l not in list("12345678"):
+                die("%s is not a valid lane." % l)
+
+        # Collector for projects removed
+        projects = set()
+        lanes = set(lanes)
+
+        try:
+            # Deleting the FASTQ that hasn't been renamed
+            projects.update(delete_d_fastq(os.path.join(output_dir, 'demultiplexing'), lanes, log=log))
+
+            # Deleting the FASTQ that is already post-processed
+            projects.update(delete_p_fastq(output_dir, lanes, log=log))
+
+            # Deleting the [other stuff]
+            pass
+
+            # Put anything I deleted into projects_pending.txt
+            with open(os.path.join(output_dir, 'projects_pending.txt'), 'a') as pp_fh:
+                for p in projects:
+                    print(p, file=pp_fh)
+
+                log("# DONE: %s projects added to projects_pending.txt" % len(projects))
+
+
+        except BaseException as e:
+            # Trap BaseException so we log death-by-SIGINT
+            log("# EXCEPTION: %s" % e)
+            raise
+
+def delete_p_fastq(path, lanes, **kwargs):
+    """Delete FASTQ from the top-level dir and return a list of the projects
+       impacted.
+       Files in here match [0-9]{6}_[^_]+_[0-9]+_[^_]+_(.)_[^_]+(_[12]|)\.fastq\.gz
+       where $1 is the lane number.
+    """
+    return delete_fastq( path, lanes,
+                         re.compile(r'^[0-9]{6}_[^_]+_[0-9]+_[^_]+_(.)_[^_]+(_[12]|)\.fastq\.gz'),
+                         **kwargs )
+
+
+def delete_d_fastq(path, lanes, **kwargs):
+    """Delete FASTQ from the demultiplexing area and return a list of the projects
+       impacted.
+       Files in here match .*_L00(.)_.\d_\d\d\d\.fastq\.gz where $1 is the lane.
+    """
+    return delete_fastq( path, lanes,
+                         re.compile(r'_L00(.)_.._\d\d\d\.fastq\.gz$'),
+                         **kwargs )
+
+
+def delete_fastq(path, lanes, match_pattern, log=lambda x: None):
+    """Generic file deleter given a path and a pattern.
+    """
+    # Find all the matching files. Simplistically, assume that any top-level directory
+    # that contains such files is a project number.
+    projects = set()
+    for root, dirs, files in os.walk(path):
+        # At the top level, only descent into directories that are numbers (ie. projects)
+        # We expect to see the unassigned reads at this level
+        if root == path:
+            dirs[:] = [ d for d in dirs if re.search(r'^[0-9]+$', d) ]
+
+        for f in files:
+            mo = re.search(match_pattern, f)
+            if mo and mo.group(1) in lanes:
+                # Pull out the project from the path (unassigned files have no project, of course!)
+                proj = root[len(path):].strip(os.path.sep).split(os.path.sep)[0]
+                if proj: projects.add( proj )
+
+                os.remove(os.path.join(root, f))
+                log("rm '%s'" % os.path.join(root, f))
+
+        # Useful for debugging
+        #    else:
+        #        if mo:
+        #            log("# lane %s is not in %s" % (mo.group(1), lanes))
+        #        else:
+        #            log("# %s does not match %s" % (f, match_pattern))
+
+    # Now remove empty directories. We only want to look at those in projects.
+    for proj in projects:
+        for root, dirs, files in os.walk(os.path.join(path, proj), topdown=False):
+            try:
+                os.rmdir(root)
+                log("rmdir '%s'" % root)
+            except Exception:
+                pass # Assume it was non-empty.
+
+    return projects
+
+if __name__ == '__main__':
+    main(*sys.argv[1:])
---
+++ test/test_bcl2fastq_cleanup.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+import unittest
+from unittest.mock import Mock, patch
+import sys, os, glob, re
+from tempfile import mkdtemp
+from shutil import rmtree, copytree
+from io import StringIO
+from os import remove
+
+# Adding this to sys.path makes the test work if you just run it directly.
+sys.path.insert(0,'.')
+from BCL2FASTQCleanup import main as c_main
+
+if os.environ.get('VERBOSE', '0') != '0':
+    _c_main = c_main
+    def c_main(*a, **k) :
+        try:
+            _c_main(*a, **k)
+        except:
+            raise
+        finally:
+            print("After running c_main(*{}, **{})".format(a, k))
+            os.system("tree")
+            os.system("cat {}/cleanup.log".format(a[0]))
+
+class TestBCL2FASTQCleanup(unittest.TestCase):
+
+    def setUp(self):
+        # Look for test data relative to this Python file
+        self.seqdata_dir = os.path.abspath(os.path.dirname(__file__) + '/demuxed_examples')
+
+        #Always make a fresh temporary folder to be working in
+        oldcwd = os.getcwd()
+        temp_dir = mkdtemp()
+        os.chdir(temp_dir)
+
+        def cleanup():
+            os.chdir(oldcwd)
+            rmtree(temp_dir)
+
+        # See the errors in all their glory
+        self.maxDiff = None
+
+    def copy_run(self, run_id):
+        copytree( os.path.join(self.seqdata_dir, run_id),
+                  run_id,
+                  symlinks=True )
+
+    def test_cleanup_1(self):
+        """ Run 170329_K00166_0198_BHJ53FBBXX has three projects.
+            Let's clean lanes 1 and 8
+        """
+        run_id = '170329_K00166_0198_BHJ53FBBXX'
+        self.copy_run(run_id)
+
+        #Sanity-check
+        self.assertTrue(os.path.exists(run_id + '/' + run_id + '_1_unassigned_1.fastq.gz'))
+        self.assertTrue(os.path.exists(run_id + '/' + run_id + '_2_unassigned_1.fastq.gz'))
+        self.assertTrue(os.path.exists(run_id + '/demultiplexing/Undetermined_S0_L001_R2_001.fastq.gz'))
+        self.assertTrue(os.path.exists(run_id + '/demultiplexing/Undetermined_S0_L002_R2_001.fastq.gz'))
+        c_main(run_id, '1', '8')
+
+        with open(run_id + '/projects_pending.txt') as fh:
+            projs = sorted([ l.rstrip('\n') for l in fh ])
+
+            self.assertEqual(projs, ['10799', '10809'])
+
+        # Did the right files get cleaned?
+        self.assertFalse(os.path.exists(run_id + '/' + run_id + '_1_unassigned_1.fastq.gz'))
+        self.assertTrue(os.path.exists(run_id + '/' + run_id + '_2_unassigned_1.fastq.gz'))
+        self.assertFalse(os.path.exists(run_id + '/demultiplexing/Undetermined_S0_L001_R2_001.fastq.gz'))
+        self.assertTrue(os.path.exists(run_id + '/demultiplexing/Undetermined_S0_L002_R2_001.fastq.gz'))
+
+    def test_cleanup_badargs(self):
+        """ Run 170329_K00166_0198_BHJ53FBBXX has three projects.
+            Let's ask to clean lanes 8 and 9
+        """
+        run_id = '170329_K00166_0198_BHJ53FBBXX'
+        self.copy_run(run_id)
+
+        self.assertRaises(SystemExit, c_main, run_id)
+        with open(run_id + '/cleanup.log') as lfh:
+            self.assertTrue("No lanes specified" in list(lfh)[-1])
+
+        self.assertRaises(SystemExit, c_main, run_id, '8', '9')
+        with open(run_id + '/cleanup.log') as lfh:
+            self.assertTrue("not a valid lane" in list(lfh)[-1])
+
+        # With non-existent run
+        self.assertRaises(FileNotFoundError, c_main, "nosuchrun", '4', '5')
