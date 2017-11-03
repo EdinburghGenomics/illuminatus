2b369fb4e2ecfb9610297c08ab7713bd79cbedd3
Tim Booth <tim.booth@ed.ac.uk>
2017-11-03 15:16:07 +0000

    Helper script to find the last error.

---
+++ test/end_to_end/showerr.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -eu
+
+# Examine why a QC failed. Basically, find the last job that failed and
+# print the slurm error log.
+echorun() { echo "$*" ; "$@" ; }
+
+if [ -e "`dirname $BASH_SOURCE`"/../../environ.sh ] ; then
+    pushd "`dirname $BASH_SOURCE`/../.." >/dev/null
+    source ./environ.sh
+    popd >/dev/null
+fi
+
+# Same as for redo, work out which run we thing we're looking at
+if [ -n "${1:-}" ] ; then
+    myrun="${SEQDATA_LOCATION}/$1/pipeline"
+elif [ -e "$SEQDATA_LOCATION/${RUN_NAME_REGEX:-}" ] ; then
+    # Run name regex is set and specifies a single run.
+    # Maybe I should grep this properly?
+    myrun="${SEQDATA_LOCATION}/${RUN_NAME_REGEX}/pipeline"
+else
+    # Guess the last run I was tinkering with.
+    myrun="`ls -dt $SEQDATA_LOCATION/*/pipeline | head -n 1`"
+fi
+
+echo "==> Last few errors in $myrun/pipeline.log <==" | env GREP_COLORS='mt=01;35' grep --color=always '.*'
+
+grep --color=always '^Error executing rule' "$myrun"/pipeline.log | tail -n 5
+echo
+
+lastfail=`grep '^Error executing rule' "$myrun"/pipeline.log | grep -o 'external: [0-9]\+' | awk '{print $2}' | tail -n 1`
+
+slurm_dir="$FASTQ_LOCATION/$(basename $(dirname "$myrun"))"/slurm_output
+tail -v -n 50 "$slurm_dir/"*.$lastfail.??? | env GREP_COLORS='mt=01;35' grep --color=always '==>.*\|^'
