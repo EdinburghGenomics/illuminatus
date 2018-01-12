eb83b64ae7f54b222daf48c1b587051d4e7a1f2b
Tim Booth <tim.booth@ed.ac.uk>
2018-01-12 11:18:47 +0000

    Add pedantic release scripts as featured on our other codebases.

---
+++ get_latest_tag.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# This helps you get a new version from GIT and deploy it
+# to ~pipeline/illuminatus on GSEG.  Change to that directory
+# before running it.
+# It is "safe" in that it won't clobber anything.  I promise.
+
+# To prep for first release:
+#  git init --bare --shared git_repo
+#  git --git-dir=git_repo remote add origin git@gitlab.genepool.private:production-team/pacbio_scripts.git
+#  git --git-dir=git_repo fetch --all
+
+die(){ echo $@ ; exit 1 ; }
+runcmd(){ echo "RUNNING: $@" ; "$@" ; }
+
+# Sanity checking stage...
+[ -d git_repo ] || die "No git_repo folder."\
+    "This script should normally be run in /lustre/home/pipeline/illuminatus/"
+
+# Update the git_repo.  This doesn't affect any actual working files.
+# Note - if the tag doesn't appear to be fetched then make sure you
+# tagged it on the master branch!
+runcmd git --git-dir=git_repo fetch --all
+runcmd git --git-dir=git_repo repack -ad
+
+#Note this assumes the tags are all of the form v0.0.0 and ignores others.
+#The top tag will be the latest.
+listtags(){
+    git --git-dir=git_repo log --tags --simplify-by-decoration --pretty="format:%d" |\
+        grep -o 'tag: v[0-9][^,)]*' | awk '{print $2}'
+}
+
+# Find the latest tag in the repo
+latest_tag="`listtags | head -n1`"
+
+# Find the latest tag from the repo for which there is a directory
+latest_checked_out="NONE"
+for tag in `listtags` ; do
+    if [ -e "$tag" ] ; then
+        latest_checked_out="$tag"
+        break
+    fi
+done
+
+# Check we saw something
+[ -z "$latest_tag" ] && die "No tags found in GIT"
+
+# See if the latest tag already exists as a directory
+[ -d "$latest_tag" ] && die "The latest tag - $latest_tag - is already checked out."
+
+# So we know there is no folder for latest_tag, it's safe to create one and populate it
+runcmd mkdir "$latest_tag"
+runcmd git --git-dir=git_repo --work-tree="$latest_tag" checkout -f tags/"$latest_tag"
+
+# Finally, alert the user if there were local changes in $latest_checked_out
+
+# Sanity check
+[ -d "$latest_checked_out" ] || die "Could not read folder for tag $latest_checked_out"
+
+if git --git-dir=git_repo --work-tree="$latest_checked_out" diff tags/"$latest_checked_out" -- | grep -q '' ; then
+    echo "WARNING - unexpected difference between the files in $latest_checked_out/ and the matching tag in GIT!"
+    echo "Maybe somebody did a cheeky in-place edit?"
+    echo "To check:"
+    echo "  git --git-dir=git_repo --work-tree=$latest_checked_out diff tags/$latest_checked_out"
+    echo
+fi
+
+echo "Checked out version $latest_tag.  If you are happy, re-point the symlink now:"
+echo "  cd `pwd`"
+echo "  rm current ; ln -s $latest_tag current"
---
+++ tag_new_release.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+set -e
+set -u
+
+# Shell script to help you tag a new release of the code.
+
+# Note that this script might annoy you because is is very picky.
+# It is meant to be picky!
+# In particular - commit all changes to master and push
+#               - add all untracked files to .gitignore
+#               - don't edit version.txt by hand
+
+# What it does...
+# 1) Check that I'm on branch master
+# 2) Check that work dir is clean
+# 3) Check that version.txt corresponds to the latest tag
+# 4) Suggest a new version tag
+#   ---are you sure---
+# 5a) echo $newversion > version.txt && git commit
+# 5b) git tag
+# 6) git push --tags
+# 7) remind the user to now do
+#     cd /ifs/software/linux_x86_64/qc_tools_python/ ; `pwd`/get_latest_tag.sh
+
+
+listtags(){
+    git log --tags --simplify-by-decoration --pretty="format:%d" |\
+        grep -o 'tag: v[0-9][^,)]*' | awk '{print $2}'
+}
+
+die(){
+    echo "$@" ; exit 1
+}
+
+yesno(){
+    while true ; do
+        read -p '[y/n] ' -n1 answer
+        if [ "$answer" = 'y' -o "$answer" = 'Y' ] ; then
+            echo yes ; echo >&2 ; break
+        elif [ "$answer" = 'n' -o "$answer" = 'N' ] ; then
+            echo no ; echo >&2 ; break
+        fi
+        echo '?' >&2
+    done
+}
+
+#1 The file .git/HEAD should contain "ref: refs/heads/master"
+# However, this is subsumed into the next test.
+#[ "`cat .git/HEAD`" = "ref: refs/heads/master" ] || \
+#    die "You do not seem to be working on the master branch."
+
+#2 - As well as being on master, all untracked files should be in .gitignore
+# and all changes should be pushed upstream.
+foo=`env LC_ALL=C git status`
+[ "$foo" = $'# On branch master\nnothing to commit, working directory clean' ] || \
+    die $'GIT reports that not all changes are pushed on the master branch...\n'"$foo"
+
+#3
+# Find the latest tag in the repo
+latest_tag="`listtags | head -n1`"
+
+latest_version_txt=`cat version.txt`
+
+[ -z "$latest_tag" -o "$latest_tag" = "v$latest_version_txt" ] || \
+    die "Version mismatch for previous tag - $latest_tag != v$latest_version_txt."$'\n' \
+        "Edit version.txt and try again."
+
+#4
+new_version=`echo "$latest_version_txt" | perl -pe 's/(\d+)$/$1+1/e'`
+
+echo "Your current version is $latest_tag."
+echo "Enter new version to release (or accept the default suggestion)"
+read -p ': v' -e -i "$new_version" actual_new_version
+
+allowedchars='0123456789._-'
+if [ -z "$actual_new_version" ] || \
+   [ "`echo "$actual_new_version" | perl -pe "tr/$allowedchars//dc"`" != "$actual_new_version" ]
+   then
+        die "Invalid or empty version string.  Can only contain $allowedchars"
+fi
+
+#5
+
+echo "Tag v$actual_new_version will now be added and pushed to the repository,"
+echo "and version.txt will be updated.  Are you sure?"
+[ "`yesno`" = yes ] || die
+
+
+
+echo "$actual_new_version" > version.txt
+
+git commit version.txt -m "Tagging new release v$actual_new_version"
+git tag "v$actual_new_version"
+
+git push --tags
+git push
+
+echo "All done.  Now do:"
+echo "  cd ~pipeline/illuminatus && `pwd`/get_latest_tag.sh"
