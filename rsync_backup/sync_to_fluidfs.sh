f35015718c38df1b7376cafbb0fcce596f2f71e7
Tim Booth <tim.booth@ed.ac.uk>
2018-02-13 17:02:47 +0000

    It works but I think it can be improved by making the bug I found into a feature.

--- rsync_backup/sync_to_fluidfs.sh
+++ rsync_backup/sync_to_fluidfs.sh
@@ -31,16 +31,18 @@ echo "Backing up data from $FASTQ_LOCATION to $BACKUP_LOCATION"
 RUN_NAME_REGEX="${RUN_NAME_REGEX:-.*_.*_.*_[^.]*}"
 RUN_NAME_REGEX="${BACKUP_NAME_REGEX:-$RUN_NAME_REGEX}"
 debug "RUN_NAME_REGEX=$RUN_NAME_REGEX"
+echo ===
 
 # Now loop through all the project in a similar way to the driver and the state reporter.
 # But note we loop through $FASTQDATA_LOCATION
-for run in "$FASTQDATA_LOCATION"/*/ ; do
+for run in "$FASTQ_LOCATION"/*/ ; do
 
+  # This also lops the trailing /, but we rely on $run still having one.
   run_name="`basename $run`"
 
   # Apply filter
   if ! [[ "$run_name" =~ ^${RUN_NAME_REGEX}$ ]] ; then
-    debug "Ignoring $run_name which does not match regex"
+    debug "Ignoring directory $run_name which does not match regex"
     continue
   fi
 
@@ -55,8 +57,9 @@ for run in "$FASTQDATA_LOCATION"/*/ ; do
 
   # If the pipeline dir is missing, sync it on suspicion.
   # Maybe I should RSYNC anyway here and not wait for final QC?
-  if [ -e "$run/seqdata/pipeline" ] && [ ! -e "$run/seqdata/pipeline/qc.finished" ] ; then
+  if [ -e "$run/seqdata/pipeline" ] && [ ! -e "$run/seqdata/pipeline/qc.done" ] ; then
     debug "Ignoring incomplete $run_name"
+    continue
   fi
 
   # If the pipeline.log is missing we have problems
@@ -67,7 +70,7 @@ for run in "$FASTQDATA_LOCATION"/*/ ; do
 
   # Comparing times on pipeline.log is probably the simplest way to see if the copy
   # is up-to-date and saves my sync-ing everything again and again
-  if rsync -nsa --itemize-changes "$run/pipeline.log" "$BACKUP_LOCATION/$run_name/pipeline.log" | grep -q . ; then
+  if rsync -nsa --itemize-changes --include='pipeline.log' --exclude='*' "$run" "$BACKUP_LOCATION/$run_name" | grep -qF pipeline.log ; then
     log_size=`stat -c %s "$run/pipeline.log"`
     debug "Detected new pipeline log activity for $run_name with log size $log_size"
   else
@@ -83,16 +86,19 @@ for run in "$FASTQDATA_LOCATION"/*/ ; do
     continue
   fi
 
-  rsync -sav --exclude=.snakemake --exclude=slurm_output --exclude=seqdata --exclude=pipeline.log \
-    "$run" "$BACKUP_LOCATION"
+  rsync -sav --exclude='**/.snakemake' --exclude='**/slurm_output' --exclude=seqdata --exclude=pipeline.log \
+    "$run" "$BACKUP_LOCATION/$run_name"
+
+  # Just to test the log catcher below we can...
+  # echo BUMP >> "$run/pipeline.log"
 
   # Now add the pipeline directory and the SampleSheets
-  rsync -sav --include=pipeline --include='SampleSheet*' --include='*.xml'
-    "$run"/seqdata "$BACKUP_LOCATION/$run_name"
+  rsync -sav --include='pipeline**' --include='SampleSheet*' --include='*.xml' --exclude='*' \
+    "$run"seqdata/ "$BACKUP_LOCATION/$run_name/seqdata"
 
   # And finally the log. Do this last so if copying was interrupted/incomplete it will be obvious.
   # If the log has changed size during the copy process it's a problem.
-  # I'm not sure how to deal with the problem but this is one way.
+  # My solution is to touch the pipeline.log so the next iteration will spot it and re-sync.
   rsync -sa --itemize-changes "$run/pipeline.log" "$BACKUP_LOCATION/$run_name/pipeline.log"
   if [ `stat -c %s "$run/pipeline.log"` != $log_size ] ; then
     echo "Log file size has changed during sync. To ensure that no new data is missed, the timestamp"
