47fe0c00e1c3656b5c37837eddfea57a9bc519ed
Tim Booth <tim.booth@ed.ac.uk>
2020-06-11 17:52:29 +0100

    Still trying to pin down that behaviour

--- doc/test_e2.sh
+++ doc/test_e2.sh
@@ -3,11 +3,12 @@ set -euo pipefail
 shopt -sq failglob
 
 echo "Some obscure BASH behaviour here"
-echo "On CentOS 7 this prints '1' and 'Caught The Exception' for 2 and 3"
+echo "On CentOS 7 this prints '1' and 'Caught The Exception' for 2 and 3 and 4"
+echo "On CentOS 8 this only works for version 4"
 
 foo(){
 
-   ( set -e ; 
+   set +e ; ( set -e ;
 
     false ## An error!
     echo "This should not print"
@@ -17,6 +18,11 @@ foo(){
    return $retval
 }
 
+set -e
 foo || echo Caught The Exception 1
+set -e
 eval foo || echo Caught The Exception 2
-{ eval foo ; } || echo Caught The Exception 3
+set -e
+{ eval foo ; true ; } || echo Caught The Exception 3
+set -e
+{ eval foo ; } ; [ $? = 0 ] || echo Caught The Exception 4
