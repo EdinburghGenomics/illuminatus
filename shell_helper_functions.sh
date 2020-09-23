0f2b1ae654abea8d8db130ca28cff50b2b5d4c2f
Tim Booth <tim.booth@ed.ac.uk>
2020-09-23 15:02:28 +0100

    Re-sync shell_helper_functions with Hesiod.

--- shell_helper_functions.sh
+++ shell_helper_functions.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
 ## Helper functions for shell scripts.
-__EXEC_DIR="${EXEC_DIR:-`basename $BASH_SOURCE`}"
+__EXEC_DIR="${EXEC_DIR:-`dirname $BASH_SOURCE`}"
 
 # All the Snakefiles have bootstrapping scripts on them, but this script
 # will run snakemake directly via the shell helper functions.
-export LOCAL_JOBS=${LOCAL_JOBS:-2}
-export SNAKE_THREADS=${SNAKE_THREADS:-200}
 export DRY_RUN=${DRY_RUN:-0}
+LOCAL_CORES=${LOCAL_CORES:-4}
+SNAKE_THREADS=${SNAKE_THREADS:-200}
+EXTRA_SNAKE_FLAGS="${EXTRA_SNAKE_FLAGS:-}"
+EXTRA_SLURM_FLAGS="${EXTRA_SLURM_FLAGS:--t 24:00}"
 
 ## Dump out the right cluster config (just now we only have one)
-function cat_cluster_yml(){
-    cat "`dirname $0`"/cluster.slurm.yml
+function cat_cluster_yaml(){
+    cat "`dirname $0`"/cluster.slurm.yaml
 }
 
 find_toolbox() {
     # The toolbox used by the pipeline can be set by setting TOOLBOX in the
     # environment (or environ.sh). Otherwise look for it in the program dir.
-    _toolbox="$( cd $(dirname "$BASH_SOURCE") && readlink -f ${TOOLBOX:-toolbox} )"
+    _toolbox="$( cd $__EXEC_DIR && readlink -f ${TOOLBOX:-toolbox} )"
     echo "$_toolbox"
 
     if ! [ -e "$_toolbox/" ] ; then
         echo "WARNING - find_toolbox - No such directory ${_toolbox}" >&2
     fi
 }
+
 
 # Functions to run a Snakefile
 find_snakefile() {
@@ -54,26 +57,27 @@ snakerun_drmaa() {
     fi
 
     # Spew out cluster.yaml
-    [ -e cluster.yml ] || cat_cluster_yml > cluster.yml
+    [ -e cluster.yaml ] || cat_cluster_yaml > cluster.yaml
 
     # Ensure Snakemake uses the right wrapper script.
     # In particular this sets TMPDIR
     _jobscript="`find_toolbox`/snakemake_jobscript.sh"
 
-    # With alternative --drmaa args and cluster.yml this could run on
+    # With alternative --drmaa args and cluster.yaml this could run on
     # SGE. See v1.4.6 for the old code that actually supported this.
     echo
     echo "Running $snakefile in `pwd` on the SLURM cluster"
     SNAKE_THREADS="${SNAKE_THREADS:-100}"
-    EXTRA_SNAKE_FLAGS="${EXTRA_SLURM_FLAGS:-}"
+    EXTRA_SNAKE_FLAGS="${EXTRA_SNAKE_FLAGS:-}"
     EXTRA_SLURM_FLAGS="${EXTRA_SLURM_FLAGS:--t 24:00}"
 
     mkdir -p ./slurm_output
     set -x
     snakemake \
          -s "$snakefile" -j $SNAKE_THREADS -p --rerun-incomplete \
-         ${EXTRA_SNAKE_FLAGS} --keep-going --cluster-config cluster.yml \
-         --jobname "{rulename}.snakejob.{jobid}.sh" \
+         ${EXTRA_SNAKE_FLAGS} --keep-going --cluster-config cluster.yaml \
+         --resources nfscopy=1 --local-cores $LOCAL_CORES --latency-wait 10 \
+         --jobname "{rulename}.snakejob.{jobid}.sh" --jobscript "$_jobscript" \
          --drmaa " ${EXTRA_SLURM_FLAGS} -p ${CLUSTER_QUEUE} {cluster.slurm_opts} \
                    -e slurm_output/{rule}.snakejob.%A.err \
                    -o slurm_output/{rule}.snakejob.%A.out \
@@ -86,9 +90,9 @@ snakerun_single() {
     snakefile=`find_snakefile "$1"` ; shift
 
     echo
-    echo "Running $snakefile in `pwd` in local mode"
+    echo "Running $snakefile in `pwd -P` in local mode"
     snakemake \
-         -s "$snakefile" -j $LOCAL_JOBS -p --rerun-incomplete \
+         -s "$snakefile" -j $LOCAL_CORES -p --rerun-incomplete ${EXTRA_SNAKE_FLAGS:-} \
          "$@"
 }
 
@@ -96,7 +100,7 @@ snakerun_touch() {
     snakefile=`find_snakefile "$1"` ; shift
 
     echo
-    echo "Running $snakefile --touch in `pwd` to update file timestamps"
+    echo "Running $snakefile --touch in `pwd -P` to update file timestamps"
     snakemake -s "$snakefile" --quiet --touch "$@"
     echo "DONE"
 }
@@ -107,6 +111,6 @@ if [ "$0" = "$BASH_SOURCE" ] ; then
 
     echo
     echo "Here is the cluster config..."
-    cat_cluster_yml
+    cat_cluster_yaml
 fi
 
